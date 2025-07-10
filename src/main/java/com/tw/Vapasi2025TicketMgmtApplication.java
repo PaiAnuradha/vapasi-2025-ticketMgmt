9b87d6dd4ddb7ec5bb7d55fed4b3c741004bcceb
anuradhapai <anuradhashenoy4@gmail.com>
2025-07-10 22:24:35 +0530

    Springboot skeleton

---
+++ src/main/java/com/tw/Vapasi2025TicketMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.tw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vapasi2025TicketMgmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vapasi2025TicketMgmtApplication.class, args);
+    }
+
+}
