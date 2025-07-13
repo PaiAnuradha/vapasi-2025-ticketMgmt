71b952775328f598cd46334918fecb2bf42f11d4
nithyaguruprasad <nith.here@gmail.com>
2025-07-13 17:29:10 +0530

    Add test class for ticket repo

--- src/main/java/com/tw/entity/Ticket.java
+++ src/main/java/com/tw/entity/Ticket.java
@@ -2,14 +2,20 @@ package com.tw.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Builder
 @Data
 @Entity
 @Table(name="ticket")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/test/java/com/tw/repository/TicketRepositoryTest.java
@@ -0,0 +1,79 @@
+package com.tw.repository;
+
+import com.tw.entity.Ticket;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class TicketRepositoryTest {
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    @Test
+    public void testSaveTicket(){
+        Ticket ticket = Ticket.builder()
+                .destination("Bengluru")
+                .source("Mysore")
+                .travelDate(LocalDate.now())
+                .build();
+        ticketRepository.save(ticket);
+        assertThat(ticket.getPnr()).isGreaterThan(0);
+    }
+
+    @Test
+    void testfindById() {
+
+        Ticket ticket = Ticket.builder()
+                .destination("Bengluru")
+                .source("Mysore")
+                .travelDate(LocalDate.now())
+                .build();
+        Ticket ticket1 = ticketRepository.save(ticket);
+        Optional<Ticket> ticketOptional = ticketRepository.findById(String.valueOf(ticket1.getPnr()));
+        assertTrue(ticketOptional.isPresent());
+        assertEquals(1, ticketOptional.get().getPnr());
+        assertEquals(ticket.getTravelDate(), ticketOptional.get().getTravelDate());
+        assertEquals("Bengluru", ticketOptional.get().getDestination());
+        assertEquals("Mysore", ticketOptional.get().getSource());
+    }
+
+    @Test
+    void testfindAll() {
+        Ticket ticketBuilder = Ticket.builder()
+                .destination("Bengluru")
+                .source("Mysore")
+                .travelDate(LocalDate.now())
+                .build();
+        Ticket ticket1 = ticketRepository.save(ticketBuilder);
+        Ticket ticketBuilder2 = Ticket.builder()
+                .destination("Bengluru")
+                .source("Mysore")
+                .travelDate(LocalDate.now())
+                .build();
+        Ticket ticket2 = ticketRepository.save(ticketBuilder2);
+
+        List<Ticket> tickets = ticketRepository.findAll();
+        assertFalse(tickets.isEmpty());
+        assertEquals(2, tickets.size());
+    }
+
+    @Test
+    void testfindAllForEmptyTable() {
+        List<Ticket> tickets = ticketRepository.findAll();
+        assertTrue(tickets.isEmpty());
+    }
+
+    @Test
+    void testfindByNonExistentId() {
+        Optional<Ticket> ticketOptional = ticketRepository.findById(String.valueOf(1234));
+        assertFalse(ticketOptional.isPresent());
+    }
+}
